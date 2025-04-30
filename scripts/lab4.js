33eb08247b0decf7292c0fb9587007ea8adb4190
AshSensei <an9simbol@gmail.com>
2025-04-30 05:38:41 -0700

    more test case fixes

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add && Number.isNaN(num1) && Number.isNaN(num2)) {
+    if (add && typeof num1 == 'number' && typeof num2 == 'number') {
         let result = 0;
 
         result = num1 + num2;
@@ -28,7 +28,7 @@ function discountPrices(prices, discount) {
     const discounted = [];
     const length = prices.length;
     let discountedPrice = 0;
-    if(prices==[] || !Number.isNaN(prices[0]) || !Number.isNaN(discount)){
+    if(length == 0 || !Array.isArray(prices)){
         return false;
     }
     for(let i = 0; i < length; i++) {
